674c86fe4fc8156fdde7af6cc8ddef3a16a8e04d
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-17 16:52:44 +0530

    Implement dummy server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-// happy coding 👻
-console.log("hello world");
+import express from "express"
+
+const port = process.env.PORT || 3000;
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello world" })
+})
+
+app.listen(port, () => console.log(`Starting server on port ${port}`))

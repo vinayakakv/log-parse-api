92609b35e960e70ae89f800f9b5e5397982f1605
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-18 15:07:21 +0530

    Generalize LogFileParser

--- src/lib/logFileParser.ts
+++ src/lib/logFileParser.ts
@@ -2,12 +2,20 @@ import { FileParser } from "./fileParser";
 import { ParseResult } from "../models/parseResult";
 
 export class LogFileParser implements FileParser {
+    private delimiter: string
+    private logLevels: string[]
+
+    constructor(args: { delimiter: string, logLevels: string[] }) {
+        this.delimiter = args.delimiter
+        this.logLevels = args.logLevels
+    }
+
     parse(content: string): ParseResult {
         return content
             .split("\n")
             .filter(line => line.length > 0)
-            .map(line => line.split(" - ").map(token => token.trim()))
-            .filter(([, logLevel,]) => logLevel === "error" || logLevel === "warn")
+            .map(line => line.split(this.delimiter).map(token => token.trim()))
+            .filter(([, logLevel,]) => this.logLevels.includes(logLevel))
             .map(([timestamp, logLevel, message]) => {
                 const { err, transactionId } = JSON.parse(message)
                 return { timestamp, logLevel, transactionId, err }

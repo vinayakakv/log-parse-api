08c58148efe091b7dc831d0e7665ce33e3032356
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-20 09:23:46 +0530

    Implement log parsing API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express"
+import multer from "multer"
+import { LogFileParser } from "./lib/logFileParser";
 
 const port = process.env.PORT || 3000;
 
 const app = express()
+const upload = multer()
+
+const parser = new LogFileParser({delimiter: " - ", logLevels: ["error", "warn"]})
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello world" })
 })
 
-app.listen(port, () => console.log(`Starting server on port ${port}`))
+app.post("/parse", upload.single("file"), (req, res) => {
+    const text = req.file?.buffer.toString() || ""
+    const parsedText = parser.parse(text)
+    res.json({success: true, data: parsedText})
+})
+
+app.listen(port, () => console.log("Listening"))
